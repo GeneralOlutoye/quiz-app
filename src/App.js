ef3395123dc79fcb9c1dd52e6b30308fee0068df
General Olutoye <donronald27@gmail.com>
2022-09-23 09:16:28 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { QuestionPage } from './questionPage/QuestionPage'
 function App() {
   return (
     <div className="App">
+//     {/* page title */}
       <h1 className='h1'>Quiz Master</h1>
       <QuestionPage />
     </div>
